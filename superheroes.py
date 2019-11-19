f0d463a21ad659d239f5515182e5ec9ae3ca310f
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2019-11-19 13:34:55 -0800

    Armor class added

--- superheroes.py
+++ superheroes.py
@@ -15,6 +15,19 @@ class Ability:
         random_value = random.randint(0,self.max_damage)
         return random_value
 
+class Armor:
+    def __init__(self, name, max_block):
+        """Instantiate instance properties.
+            name:String
+            max_block: Integer"""
+        #TODO: Create instance variables fro the values to passed in.
+        self.name = name
+        self.max_block = max_block
+
+    def block(self):
+        """Return a  random value between 0 and the initialized max_block strength."""
+        random_value = random.randint(0,self.max_block)
+        return random_value
 
 
 if __name__ == "__main__":
